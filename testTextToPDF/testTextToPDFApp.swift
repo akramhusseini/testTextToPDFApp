63e0c9f941f0421dd814a16b06114f5a07513d8b
akrammotory <93082320+akrammotory@users.noreply.github.com>
2024-11-06 16:49:19 +0300

    Initial Commit

---
+++ testTextToPDF/testTextToPDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  testTextToPDFApp.swift
+//  testTextToPDF
+//
+//  Created by Akram Husseini on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct testTextToPDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testTextToPDFTests/testTextToPDFTests.swift
@@ -0,0 +1,17 @@
+//
+//  testTextToPDFTests.swift
+//  testTextToPDFTests
+//
+//  Created by Akram Husseini on 06/11/2024.
+//
+
+import Testing
+@testable import testTextToPDF
+
+struct testTextToPDFTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
